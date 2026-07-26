5b25c171a7e65c744d57c5952a7e276de6cd5da3
anjaligajith935-ops <anjaligajith935@gmail.com>
2026-07-26 11:58:29 +0530

    operators and conditionals

---
+++ session-13.js
@@ -0,0 +1,184 @@
+// console.log(2+3);
+// let name="Arun";
+// console.log("name=",name)
+// console.log(typeof name)
+
+// let age="35";
+// let age=35;
+// let age= Number("35")
+// console.log(typeof age)
+// console.log(Number("abc"))
+// console.log(Number(true))
+// console.log(Number(false))
+
+// console.log(typeof String(false))
+// let city; //undefined
+// console.log(typeof null)
+// console.log(String(undefined))
+// console.log(Boolean(1))
+// console.log(Boolean(true))
+// console.log(Boolean("1"))
+// console.log(Boolean(null))
+// console.log(Boolean(""))
+// console.log("10"+2)
+// console.log("10"+2+3)
+// console.log("true"+2+3)
+// console.log("18"-2)
+// let cal="16"-3
+// console.log(typeof cal,cal)
+// console.log("10"*2)
+// console.log("10"/5)
+
+
+// arithmetic operators + - * / %
+// console.log(10+10)
+// console.log(10%2)
+// console.log(10*2)
+// console.log(10/2)
+// console.log(10%2)
+// console.log(10-2)
+
+
+// comparison operators > < <= >= == != ===
+// console.log(10>2)
+// console.log(10<2)
+// console.log(10>=2)
+// console.log(10<=2)
+// console.log(10==2)
+// console.log(10!=2)
+// console.log(10=="10")
+// console.log(10==="10")
+
+// assignment operator
+// let score=98
+// score+=5
+// console.log(score)
+// let score=98
+// score-=5
+// console.log(score)
+// let scores=98
+// scores*=5
+// console.log(scores)
+// let score1=98
+// score1/=5
+// console.log(score1)
+// let score2=98
+// score2%=5
+// console.log(score2)
+
+// logical operators
+
+// console.log(true&false)
+// console.log(true&true)
+// console.log(false&false)
+// console.log(false&true)
+
+// console.log(true||false)
+// console.log(true||true)
+// console.log(false||false)
+// console.log(false||true)
+//  console.log(10>2||5<2);
+
+// statements
+// if , if else , if else if
+// let score=50
+// if (score<48){
+//     console.log("passed")
+// }
+// console.log("all the best")
+
+// if (score>40){
+// console.log("passed")
+// }else{
+//     console.log("failed")
+// }
+// console.log("all the best")
+
+// let score=101
+// if (score>=90){
+//     console.log("A+")
+// }else if(score>=80){
+//     console.log("A")
+// }else if(score>=70){
+//     console.log("B+")
+// }else if(score>=60){
+//     console.log("B")
+// }else if(score>=50){
+//     console.log("C+")
+// }else if(score>=40){
+//     console.log("C")
+// }else if(score>=30){
+//     console.log("D+")
+// }else if(score>=20){
+//     console.log("D")
+// }else if(score>=10){
+//     console.log("E+")
+// }else if(score>=5){
+//     console.log("E")
+// }                  
+// console.log("all the best")
+
+//switch
+
+// let week=8;
+// switch(week) {
+//     case 1:
+//         console.log("monday")
+//         break;
+//         case 2:
+//         console.log("tuesday")
+//         break;
+//         case 3:
+//         console.log("wednesday")
+//         break;
+//         case 4:
+//         console.log("thursday")
+//         break;
+//         case 5:
+//         console.log("friday")
+//         break;
+//         case 6:
+//         console.log("saturday")
+//         break;
+//         case 7:
+//         console.log("sunday")
+//         break;
+//         default:console.log("invalid input")
+//         }
+
+        // let score=76
+        // switch(score>0) {
+        //     case score>=90:
+        //         console.log("A+")
+        //         break;
+        //         case score>=80:
+        //         console.log("A")
+        //         break;
+        //         case score>=70:
+        //         console.log("B+")
+        //         break;
+        //         case score>=60:
+        //         console.log("B")
+        //         break;
+        //         case score>=50:
+        //         console.log("C+")
+        //         break;
+        //         case score>=40:
+        //         console.log("C")
+        //         break;
+        //         case score>=30:
+        //         console.log("D+")
+        //         break;
+        //         case score>=20:
+        //         console.log("D")
+        //         break;
+        //          case score>=10:
+        //         console.log("E+")
+        //         break;
+        //          case score>=20:
+        //         console.log("E")
+        //         break;
+        // }
+
+
+
